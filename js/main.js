f7a1027ac1cf4e51a5b5ba575e8545e8c7fbfa5b
Sehr06 <lose24wo@student.ju.se>
2025-02-21 11:17:47 +0100

    interactive code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("contactForm").addEventListener("submit", function (event) 
     {event.preventDefault();
         document.getElementById("message").textContent = "Thank you!";
     });
-});
